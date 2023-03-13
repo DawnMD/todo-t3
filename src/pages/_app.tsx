50a9b82694910c981d12f6ae4ecaf8e94831ba18
Mainak Das <mainakdas104@gmail.com>
2023-03-13 23:16:08 +0530

    Feat: Add font for app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,13 +6,34 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
+import localFont from "next/font/local";
+
+const calSans = localFont({
+  src: "../../public/fonts/CalSans.woff2",
+  variable: "--cal-sans",
+  display: "swap",
+});
+
+const matter = localFont({
+  src: [
+    {
+      path: "../../public/fonts/Matter-Regular.woff",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  display: "swap",
+});
+
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <div className={`${calSans.variable} ${matter.className} antialiased`}>
+        <Component {...pageProps} />
+      </div>
     </SessionProvider>
   );
 };
